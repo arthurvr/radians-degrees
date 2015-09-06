eeef982a9afbd4f1b0cc6ab08e1db4f1e19999c4
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 21:37:11 +0200

    Use AVA for testing

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var floatEqual = require('float-equal');
 var degrees = require('./');
 
-it('should convert radians to degrees', function () {
-	assert(floatEqual(degrees(1), 57.29577951308232));
-	assert(floatEqual(degrees(0.1), 5.729577951308233));
-	assert(floatEqual(degrees(Math.PI), 180));
+test('should convert radians to degrees', function (t) {
+	t.true(floatEqual(degrees(1), 57.29577951308232));
+	t.true(floatEqual(degrees(0.1), 5.729577951308233));
+	t.true(floatEqual(degrees(Math.PI), 180));
+
+	t.end();
 });
 
-it('should throw when passing a not-numeric value', function () {
-	assert.throws(function () {
+test('should throw when passing a not-numeric value', function (t) {
+	t.throws(function () {
 		degrees({});
 	});
+
+	t.end();
 });
