5da35becbbbf575e243427f53778c1e87d313e2b
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 11:22:17 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-'use strict';
-var test = require('ava');
-var floatEqual = require('float-equal');
-var degrees = require('./');
+import test from 'ava';
+import floatEqual from 'float-equal';
+import degrees from './';
 
-test('should convert radians to degrees', function (t) {
+test('should convert radians to degrees', t => {
 	t.true(floatEqual(degrees(1), 57.29577951308232));
 	t.true(floatEqual(degrees(0.1), 5.729577951308233));
 	t.true(floatEqual(degrees(Math.PI), 180));
@@ -11,8 +10,8 @@ test('should convert radians to degrees', function (t) {
 	t.end();
 });
 
-test('should throw when passing a not-numeric value', function (t) {
-	t.throws(function () {
+test('should throw when passing a not-numeric value', t => {
+	t.throws(() => {
 		degrees({});
 	});
 
